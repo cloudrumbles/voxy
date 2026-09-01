2dae4a87647dd326cfb455a4397690471ed69986
cloudrumbles <shahriyyah@gmail.com>
2026-09-02 00:25:52 +0800

    fix: remap installed mixins and preserve Forge stairs transform

--- src/main/java/me/cortex/voxy/client/mixin/minecraft/MixinStairBlock.java
+++ src/main/java/me/cortex/voxy/client/mixin/minecraft/MixinStairBlock.java
@@ -1,25 +1,27 @@
 package me.cortex.voxy.client.mixin.minecraft;
 
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-
 import me.cortex.voxy.client.core.util.StairBlockCooked;
 import net.minecraft.world.level.block.StairBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(StairBlock.class)
 public class MixinStairBlock implements StairBlockCooked {
     @Shadow
-    BlockState baseState;
+    @Final
+    private BlockState baseState;
 
     @Override
     public BlockState setBaseWaterlogged(BlockState blockState) {
-        if (baseState.hasProperty(BlockStateProperties.WATERLOGGED)) {
-            return baseState.setValue(BlockStateProperties.WATERLOGGED,
-                    blockState.getValue(BlockStateProperties.WATERLOGGED));
-        } else {
-            return baseState;
+        if (this.baseState.hasProperty(BlockStateProperties.WATERLOGGED)) {
+            return this.baseState.setValue(
+                    BlockStateProperties.WATERLOGGED,
+                    blockState.getValue(BlockStateProperties.WATERLOGGED)
+            );
         }
+        return this.baseState;
     }
 }
