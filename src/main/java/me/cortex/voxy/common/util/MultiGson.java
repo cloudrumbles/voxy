6fe8c28d6a660d8d48ab72ba32ec00e8df696195
cloudrumbles <shahriyyah@gmail.com>
2026-09-03 12:33:01 +0800

    fix: support Gson 2.8.9 in MultiGson

--- src/main/java/me/cortex/voxy/common/util/MultiGson.java
+++ src/main/java/me/cortex/voxy/common/util/MultiGson.java
@@ -3,15 +3,21 @@ package me.cortex.voxy.common.util;
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
 import java.lang.reflect.Modifier;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
 
 public class MultiGson {
     private final List<Class<?>> classes;
     private final Gson gson;
-    private MultiGson(Gson gson,  List<Class<?>> classes) {
+
+    private MultiGson(Gson gson, List<Class<?>> classes) {
         this.gson = gson;
         this.classes = classes;
     }
@@ -23,35 +29,37 @@ public class MultiGson {
         }
         for (var obj : objects) {
             if (obj == null) {
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException("Input objects cannot be null");
             }
-            int i = this.classes.indexOf(obj.getClass());
-            if (i == -1) {
+            int index = this.classes.indexOf(obj.getClass());
+            if (index == -1) {
                 throw new IllegalArgumentException("Unknown object class: " + obj.getClass());
             }
-            if (map[i] != null) {
+            if (map[index] != null) {
                 throw new IllegalArgumentException("Duplicate entry classes");
             }
-            map[i] = obj;
+            map[index] = obj;
         }
 
         var json = new JsonObject();
         for (Object entry : map) {
-            this.gson.toJsonTree(entry).getAsJsonObject().asMap().forEach((i,j) -> {
-                if (json.has(i)) {
-                    throw new IllegalArgumentException("Duplicate name inside unified json: " + i);
+            JsonObject entryJson = this.gson.toJsonTree(entry).getAsJsonObject();
+            for (Map.Entry<String, JsonElement> field : entryJson.entrySet()) {
+                if (json.has(field.getKey())) {
+                    throw new IllegalArgumentException(
+                            "Duplicate name inside unified json: " + field.getKey());
                 }
-                json.add(i, j);
-            });
+                json.add(field.getKey(), field.getValue());
+            }
         }
         return this.gson.toJson(json);
     }
 
     public Map<Class<?>, Object> fromJson(String json) {
-        var obj = this.gson.fromJson(json, JsonObject.class);
+        var object = this.gson.fromJson(json, JsonObject.class);
         LinkedHashMap<Class<?>, Object> objects = new LinkedHashMap<>();
-        for (var cls : this.classes) {
-            objects.put(cls, this.gson.fromJson(obj, cls));
+        for (var type : this.classes) {
+            objects.put(type, this.gson.fromJson(object, type));
         }
         return objects;
     }
@@ -59,9 +67,11 @@ public class MultiGson {
     public static class Builder {
         private final LinkedHashSet<Class<?>> classes = new LinkedHashSet<>();
         private final GsonBuilder gsonBuilder;
+
         public Builder(GsonBuilder gsonBuilder) {
             this.gsonBuilder = gsonBuilder;
         }
+
         public Builder() {
             this(new GsonBuilder()
                     .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
@@ -69,8 +79,8 @@ public class MultiGson {
                     .excludeFieldsWithModifiers(Modifier.PRIVATE));
         }
 
-        public Builder add(Class<?> clz) {
-            if (!this.classes.add(clz)) {
+        public Builder add(Class<?> type) {
+            if (!this.classes.add(type)) {
                 throw new IllegalArgumentException("Class has already been added");
             }
             return this;
@@ -80,7 +90,6 @@ public class MultiGson {
             return new MultiGson(this.gsonBuilder.create(), new ArrayList<>(this.classes));
         }
     }
-
 
     private static final class A {
         public int a;
@@ -99,8 +108,8 @@ public class MultiGson {
     public static void main(String[] args) {
         var gson = new Builder().add(A.class).add(B.class).build();
         var a = new A();
-        a.c =11;
+        a.c = 11;
         var b = new B();
-        System.out.println(gson.fromJson(gson.toJson(a,b)));
+        System.out.println(gson.fromJson(gson.toJson(a, b)));
     }
 }
